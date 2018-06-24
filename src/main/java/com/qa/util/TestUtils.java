43fb725f4526954b2f44a2de6172925b10d2930c
bkumar <bhunesh.kumar@ansys.com>
2018-06-24 19:58:47 +0530

    first git commit for my selenium

---
+++ src/main/java/com/qa/util/TestUtils.java
@@ -0,0 +1,11 @@
+package com.qa.util;
+
+public class TestUtils {
+
+	public static String vara = "variable definition";
+	
+	public void demoUtilityMethod(){
+		System.out.println("this is demo utility method");
+	}
+
+}
