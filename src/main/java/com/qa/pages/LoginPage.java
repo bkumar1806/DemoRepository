61430cc75a526896972d80b92e59079df3f5edc4
bkumar <bhunesh.kumar@ansys.com>
2018-06-24 21:45:50 +0530

    added searchpage and additional function in login page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,5 +5,9 @@ public class LoginPage {
 	public LoginPage(){
 		System.out.println("this is from log in page");
 	}
+	
+	public LoginPage(String email){
+		System.out.println("this is from log in page" + email);
+	}
 
 }
